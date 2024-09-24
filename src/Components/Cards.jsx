fd36f8298f60bceef3459d963e1d40f1fd88aca2
Utkarsh Singh <utkarsh.singh6113@gmail.com>
2024-09-24 23:08:47 +0530

    bugs fix

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Cards = () => {
     return (
         <div className=' py-14 flex justify-center gap-2'>
-            <div className="h-[25rem] rotate-[12deg] grayscale-[50%] hover:grayscale-0 duration-200 cursor-pointer w-[23rem] hover:w-[50rem] hover:rotate-0 bg-[url('../public/card2.jpg')] bg-no-repeat bg-center rounded-[2rem] bg-cover bg-blue-100 shadow-xl">
+            <div className="h-[25rem] rotate-[12deg] grayscale-[50%] hover:grayscale-0 duration-200 cursor-pointer w-[23rem] hover:w-[50rem] hover:rotate-0 bg-[url('../Assets/card2.jpg')] bg-no-repeat bg-center rounded-[2rem] bg-cover bg-blue-100 shadow-xl">
             </div>
             <div className="h-[25rem] z-10 grayscale-[50%] hover:grayscale-0 duration-200 cursor-pointer w-[23rem] bg-[url('../public/card1.jpg')] shadow-xl hover:w-[50rem] bg-no-repeat bg-center rounded-[2rem] bg-cover bg-blue-100">
             </div>
